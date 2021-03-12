042ff05b40067865b524f1bac15a7d0f9fde6511
wilson kao <wilkao036@gmail.com>
2021-03-12 20:58:33 +0800

    setting cors and helmet

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 const db = require('./db');
 
 const models = require('./models');
-
+const helmet = require('helmet');
+const cors = require('cors');
 
 const DB_HOST = process.env.DB_HOST;
 const port = process.env.PORT || 4000;
@@ -18,14 +19,19 @@ const app = express();
 db.connect(DB_HOST);
 
 const server = new ApolloServer({
-    typeDefs, resolvers, context: ({ req }) => {
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
         const token = req.headers.authorization;
         const user = getUser(token);
         console.log('user', user);
         return { models, user };
-    }
+    },
 });
 server.applyMiddleware({ app, path: '/api' });
+
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('hello world33'));
 app.listen({ port }, () => console.log(`port is ${port}`));
@@ -39,4 +45,4 @@ const getUser = token => {
             throw new Error('session error');
         }
     }
-}
+};
